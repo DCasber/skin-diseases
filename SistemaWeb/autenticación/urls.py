312b677712a792db6f65298a1ec797b3b74f6fa9
David Castillo Berná <davidcastillo200.dc@gmail.com>
2021-12-12 19:36:56 +0100

    features: restricción admin, ruteo admin, autenticación, cierre sesión

--- SistemaWeb/autenticación/urls.py
+++ SistemaWeb/autenticación/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import inicioSesion, registro, autenticacion
+from .views import inicioSesion, Registro, autenticacion, cierreSesion
+
+from django.conf.urls import url
+
+from django.contrib import admin
+from django.views.defaults import page_not_found
+from .views import admin_view
+admin.site.admin_view = admin_view
 
 urlpatterns = [
     path('', inicioSesion, name="inicioSesion"),
-    path('registro/', registro, name="registro"),
+    path('registro/', Registro.as_view(), name="registro"),
     path('auth/', autenticacion, name="autenticación"),
+    path('logout/', cierreSesion, name="cierreSesionUsuario"),
+    path('admin/logout/', cierreSesion, name="cierreSesionAdmin"),
+    
+    url(r'^admin/login/', page_not_found),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/login/', page_not_found),
 ]
--- SistemaWeb/autenticación/views.py
+++ SistemaWeb/autenticación/views.py
@@ -1,34 +1,103 @@
 from django.forms.forms import Form
 from django.shortcuts import render, redirect
 from .forms import FormularioInicioSesion, FormularioRegistro
+from django.views import View
+
+from functools import update_wrapper
+from django.http import Http404
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 
+def admin_view(view, cacheable=False):
+    """
+    Overwrite the default admin view to return 404 for not logged in users.
+    """
+    def inner(request, *args, **kwargs):
+        if not request.user.is_active and not request.user.is_staff or request.user.is_active and not request.user.is_staff:
+            raise Http404()
+        return view(request, *args, **kwargs)
+
+    if not cacheable:
+        inner = never_cache(inner)
+
+    # We add csrf_protect here so this function can be used as a utility
+    # function for any view, without having to repeat 'csrf_protect'.
+    if not getattr(view, 'csrf_exempt', False):
+        inner = csrf_protect(inner)
+
+    return update_wrapper(inner, view)
+
+
 def inicioSesion(request):
 
-    formulario = FormularioInicioSesion()
+    if request.user and request.user.is_authenticated:
 
-    error = False
+        if request.user.is_superuser:
+            return redirect("/admin/")
+        else:
+            return redirect("/home/")
 
-    return render(request, "autenticación/inicioSesión.html", {"formulario": formulario, "error": error})
-
-
-def registro(request):
-
-    if request.method == "GET":
-        formulario = FormularioRegistro()
-
+    else:
         error = False
+        if request.GET.get('error'):
+            error = True
 
-        return render(request, "autenticación/registro.html", {"formulario": formulario, "error": error})
+        formulario = FormularioInicioSesion()
 
-    elif request.method == "POST":
-        return redirect("")
+        return render(request, "autenticación/inicioSesión.html", {"formulario": formulario, "error": error})
+
+
+class Registro(View):
+
+    def get(self, request):
+
+        if request.user and request.user.is_authenticated:
+
+            if request.user.is_superuser:
+                return redirect("/admin/")
+            else:
+                return redirect("/home/")
+
+        else:
+
+            formulario = FormularioRegistro()
+
+            return render(request, "autenticación/registro.html", {"formulario": formulario})
+        
+    def post(self, request):
+        ## TODO: Comprobaciones del formulario de registro (ejs: inyección sql, existe ya el usuario (nombre), contraseñas iguales)
+    
+        ## TODO: Inserción de nuevo usuario. Contraseña con salting. Mensaje de confirmación al email. Mensaje al usuario de si el registro a salido bien o no. De salir bien mensaje de que debe confirmar su cuenta a través de su email. Hasta que no lo haga no podrá logearse, (o bien se introduce primero con un campo boolean a falso o debemos pasar los datos de alguna manera) (como hacerlo automatico?)
+    
+        return render(request, "autenticación/inicioSesión.html")
 
 
 def autenticacion(request):
 
-    #TODO: AUTENTICACIÓN
+    usuario = authenticate(request, username=request.POST.get(
+        "nombre"), password=request.POST.get("password"))
 
-    return redirect("/home/")
+    if usuario is not None:
+
+        login(request, usuario)
+
+        if usuario.is_superuser:
+            return redirect("/admin/")
+        else:
+            return redirect("/home/")
+
+    else:
+
+        return redirect("/?error=1")
+
+
+def cierreSesion(request):
+
+    logout(request)
+
+    return redirect("/")
